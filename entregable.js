5ed35ee47cb0d87e5708a4a3d26248cdb99d34a1
EmmaBruno <72508475+EmmaBruno@users.noreply.github.com>
2021-07-31 06:48:42 -0300

    Update entregable.js

--- entregable.js
+++ entregable.js
@@ -1,40 +1,42 @@
 class Usuario {
 
-    constructor (nombre, apellido, nombrelibro, autor, mascota) {
+    constructor (nombre, apellido) {
         this.nombre = nombre;
         this.apellido = apellido;
-        this.libros = [{
-            'nombre': nombrelibro,
-            'autor': autor
-        }]
-        this.mascotas = [
-            mascota
-        ]
+        this.libros = []
+        this.mascotas = []
     }
 
     getFullName() {
-        console.log(`${this.nombre} ${this.apellido}`);
+        console.log(`El nombre completo del usuario es: ${this.nombre} ${this.apellido}`);
     }
 
-    addMascota(String) {
-        mascotas.push(String)
+    addMascota(nombreMascota) {
+        this.mascotas.push(nombreMascota);
     }
 
     countMascotas() {
-        totalmascotas = this.mascotas
-        console.log(totalmascotas)
+        console.log(`la catidad de mascotas de ${this.nombre} son: `)
+        console.log(this.mascotas.length)
     }
 
-    addBook(nuevolibro, nuevoautor) {
-        this.libros.push(nuevolibro, nuevoautor)
+    addBook(titulo, autor) {
+        this.libros.push({titulo, autor})
     }
 
     getBookNames() {
-        libros.map(libros => libros.nombrelibro)
+        this.libros.map((libro)=>console.log(libro.titulo))
     }
 }
 
-let usuario = new Usuario ('Juan', 'Perez', 'El señor de las moscas', 'William Golding', 'perro')
+let usuario = new Usuario ('Juan', 'Perez')
 console.log(usuario)
 usuario.getFullName()
+usuario.addMascota('gato')
 usuario.countMascotas()
+usuario.addMascota('perro')
+usuario.countMascotas()
+usuario.addBook('El fugitivo', 'Stephen King')
+usuario.addBook('Tormenta de espadas', 'George R. R. Martin')
+usuario.addBook('La dama del lago', 'Andrzej Sapkowski')
+usuario.getBookNames()
